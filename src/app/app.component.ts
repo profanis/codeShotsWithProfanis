a065be6612f62f761b522b058a7319359aafcdac
profanis <prodromouf@gmail.com>
2021-03-23 21:32:31 +0200

    difference between rxjs subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'profanis-yt';
+export class AppComponent implements OnInit {
+  sourceSubject = new Subject();
+  behaviorSubject = new BehaviorSubject<string>('');
+  replaySubjectSource = new ReplaySubject(1);
+
+  ngOnInit(): void {
+    this.handleReplaySubject();
+    // this.handleSubject();
+    // this.handleBehaviorSubject();
+  }
+
+  private handleReplaySubject() {
+    this.replaySubjectSource.next('a value');
+    this.replaySubjectSource.next('b value');
+    this.replaySubjectSource.next('c value');
+    this.replaySubjectSource.subscribe((it) => console.warn('obs-1' + it));
+    this.replaySubjectSource.subscribe((it) => console.warn('obs-2' + it));
+  }
+
+  private handleBehaviorSubject() {
+    this.behaviorSubject.next('a value');
+    this.behaviorSubject.next('b value');
+    this.behaviorSubject.subscribe((it) => console.warn('obs-1:' + it));
+    this.behaviorSubject.subscribe((it) => console.warn('obs-2:' + it));
+  }
+
+  private handleSubject() {
+    this.sourceSubject.subscribe((it) => console.warn('obs-1' + it));
+    this.sourceSubject.subscribe((it) => console.warn('obs-2' + it));
+
+    this.sourceSubject.next('a value');
+  }
 }
