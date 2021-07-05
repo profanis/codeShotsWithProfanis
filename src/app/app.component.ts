4627ed633fc9952ca326df295c059fbde9231071
profanis <prodromouf@gmail.com>
2021-07-05 09:58:57 +0300

    implement async validator and display spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  FormControl,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'profanis-yt';
+export class AppComponent implements OnInit {
+  username!: FormControl;
+
+  private readonly usernames = ['john', 'jane'];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.username = this.fb.control(
+      null,
+      Validators.required,
+      this.userValidator()
+    );
+  }
+
+  private userExists(username: string): Observable<boolean> {
+    return of(this.usernames.includes(username)).pipe(delay(2000));
+  }
+
+  private userValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return this.userExists(control.value).pipe(
+        map((response) => (response ? { userExists: true } : null))
+      );
+    };
+  }
 }
