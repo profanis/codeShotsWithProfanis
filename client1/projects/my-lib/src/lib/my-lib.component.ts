99861f40efddb04ec9ac90575cc038bacbc885de
profanis <prodromouf@gmail.com>
2021-10-16 17:22:33 +0300

    reuse a library in different ng workspaces in a mono-repo

--- client1/projects/my-lib/src/lib/my-lib.component.ts
+++ client1/projects/my-lib/src/lib/my-lib.component.ts
@@ -2,19 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-lib',
-  template: `
-    <p>
-      Code Shots With Profanis!!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>Code Shots With Profanis!!!</p> `,
+  styles: [],
 })
 export class MyLibComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ client2/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MyLibModule } from 'my-lib';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, MyLibModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
