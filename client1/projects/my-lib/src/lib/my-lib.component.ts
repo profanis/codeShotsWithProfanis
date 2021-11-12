58e7a6bf2fcb5701e55d2ed536cc03f3a6db84b6
profanis <prodromouf@gmail.com>
2021-11-12 13:52:17 +0200

    upgrade angular

--- client1/projects/my-lib/src/lib/my-lib.component.ts
+++ client1/projects/my-lib/src/lib/my-lib.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-lib',
-  template: ` <p>Code Shots With Profanis!!!</p> `,
+  template: ` <p>Code Shots With Profanis</p> `,
   styles: [],
 })
 export class MyLibComponent implements OnInit {
