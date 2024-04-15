4881c0c0fb5bf4326b3c906dd4a33f3b49fc7445
Fanis Prodromou <prodromouf@gmail.com>
2024-04-15 13:37:42 +0300

    chore: change the list of colors

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,15 +1,15 @@
 export function randomColor() {
   const colors = [
-    '#FF0000',
-    '#00FF00',
-    '#0000FF',
-    '#FFFF00',
-    '#00FFFF',
-    '#FF00FF',
-    '#C0C0C0',
-    '#808080',
-    '#800000',
-    '#808000',
+    '#FDFEFE', // Light Gray
+    '#38A3A5', // Teal
+    '#FB8C00', // Light Orange
+    '#9D38BD', // Purple
+    '#FFF9C4', // Cream
+    '#C02942', // Deep Rose
+    '#73C6BE', // Light Blue
+    '#D87093', // Light Pink
+    '#AFEEEE', // Light Seafoam
+    '#FCF0E3', // Light Beige
   ];
 
   return colors[Math.floor(Math.random() * colors.length)];
