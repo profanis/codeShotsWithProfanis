f0f84c61f588760e25a59efc780e0c0f8f40bcaa
Fanis Prodromou <prodromouf@gmail.com>
2026-06-04 17:57:31 +0300

    chore: bump up to NG 22

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXhr } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
@@ -9,6 +9,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withXhr()),
   ],
 });
