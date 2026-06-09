5373b0e32fd3eb8a8f92beb2808904d787d991a4
Fanis Prodromou <prodromouf@gmail.com>
2026-06-09 22:47:22 +0300

    chore: bump up to angular 22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.Eager,
   imports: [],
 })
 export class AppComponent {
